91d2f9db94e286d42e45d31caf98d3e0f2c44fd7
YeJinii <yeagin09@gmail.com>
2022-03-29 18:16:55 +0900

    0616

---
+++ C++로_코테_뿌시기/Backjoon/2828.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+    
+    int N, M, J; cin>>N>>M>>J;
+
+    vector <int> v(J);
+
+    for(int i=0; i<J; i++){
+        cin>>v[i];
+    }
+
+    
+
+}
