c709b3a4f33ad06d2dd28fa1044de6e9512bb6a9
YeJinii <yeagin09@gmail.com>
2022-04-14 23:51:26 +0900

    0414 - 제발 ㅠㅠ

--- C++로_코테_뿌시기/Backjoon/10250.cpp
+++ C++로_코테_뿌시기/Backjoon/10250.cpp
@@ -5,8 +5,18 @@ int main(void){
     int T; cin>>T;
     while(T--){
         int H, W, N; cin>>H>>W>>N;
-        int floor = N%H;
-        int num = N/H+1;
-        cout<<floor*100+num<<'\n'; 
+        int count=1;
+        bool check_print=false;
+        for(int i=1; i<=W; i++){
+            for(int j=100; j<=H*100; j+=100){
+                if(count==N){
+                    cout<<j+i<<'\n';
+                    check_print = true;
+                    break;
+                }
+                count++;
+            }
+            if(check_print) break;
+        }
     }
 }
